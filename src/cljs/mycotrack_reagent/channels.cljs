cd2fa752bef55377f629a40bcef0dcbc546f74bf
ctcarrier <ctcarrier@gmail.com>
2015-04-28 20:48:19 -0400

    Pages and other cljs files

---
+++ src/cljs/mycotrack_reagent/channels.cljs
@@ -0,0 +1,14 @@
+(ns mycotrack-reagent.channels
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [reagent.core :as reagent :refer [atom]]
+            [reagent.session :as session]
+            [secretary.core :as secretary :include-macros true]
+            [goog.events :as events]
+            [goog.history.EventType :as EventType]
+            [cljsjs.react :as react]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<! chan]]
+            [clojure.set :refer [union]])
+  (:import goog.History))
+
+(def echo-chan (chan))
